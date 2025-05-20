bb582d851d6546c8706f2c1400622135302b47b2
JakubDuzi <159249308+JakubDuzi@users.noreply.github.com>
2025-05-20 09:23:11 +0000

    add_row

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ pole = [
 
 pole[1][1] = 105
 
+novy_radek = [10, 11, 12]
+novy_radek.append(13)
+pole.append(novy_radek)
+
+for radek in pole[:-1]:
+    radek.append(0)
 
 for radek in pole:
     print(radek)
